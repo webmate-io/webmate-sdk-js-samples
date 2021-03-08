b2d766c14fd670a26e847e3ea7659c4e6b524cb7
Timo Gühring <tguehring@testfabrik.com>
2021-03-08 17:21:04 +0100

    Add url-based-crossbrowser-test.test.ts and device-interaction.test.ts Update selenium-test.test.ts Update readme

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # webmate TypeScript and JavaScript SDK Samples
 
 This repository contains examples of how to use the [webmate SDK](https://github.com/webmate-io/webmate-sdk-js).
-See the REST API [Swagger documentation](https://app.webmate.io/api/swagger) for a comprehensive summary of the webmate functionalities.
+
 
 ## Executing the Tests
 This project demonstrates how to interact with the webmate API using the official webmate SDK for TypeScript and JavaScript.
 
 Before running the tests, enter your webmate credentials in `credentials.ts`.
 
-It contains two different tests. The test located in `device.test.ts` demonstrates the lifecycle of a device. 
+It contains two different tests. The test located in `device-interaction.test.ts` demonstrates the lifecycle of a device. 
 The test in `selenium-test.test.ts` creates two browser sessions by executing the same selenium test using two different browsers. 
 It then demonstrates how to use the browser sessions to start a cross browser test and query the resulting artifacts and results.   
    
@@ -17,3 +17,42 @@ To build the TypeScript tests located is `src/` run `npm run build`. This will c
 To run them execute `npm run test`. Please note that running the tests will also build them automatically.
 It is also possible to run the tests separately, using the commands `npm run device-test` and `npm run cb-test` respectively. 
 
+
+# Tests
+
+<table border="1">
+    <tr>
+        <th>Test</th>
+        <th>Frameworks</th>
+    </tr>
+    <tr>
+        <td>
+            <a href="./src/cucumber/example-page.steps.ts">Example Page Steps</a>
+        </td>
+        <td>Cucumber, Selenium, JUnit</td>
+    </tr>
+    <tr>
+        <td>
+            <a href="./src/device-interaction.test.ts">Device Interaction Test</a>
+        </td>
+        <td>Mocha, Chai</td>
+    </tr>
+    <tr>
+        <td>
+            <a href="./src/selenium-test.test.ts">Selenium Test</a>
+        </td>
+        <td>WebdriverIO, Mocha, Chai</td>
+    </tr>
+    <tr>
+        <td>
+            <a href="./src/url-based-crossbrowser-test.test.ts">URL Based Crossbrowser Test</a>
+        </td>
+        <td>Mocha, Chai</td>
+    </tr>
+</table>
+
+
+## webmate API
+
+Although, the SDK provides a number of features and convenience wrappers it doesn't exhaust the full potential of the webmate API.
+See the REST API [Swagger documentation](https://app.webmate.io/api/swagger) for a comprehensive summary of the webmate functionalities.
---
+++ src/url-based-crossbrowser-test.test.ts
@@ -0,0 +1,67 @@
+import 'mocha';
+import {should} from 'chai';
+import logger from '@wdio/logger';
+import {
+    Browser, BrowserType,
+    ExpeditionComparisonCheckBuilder, ExpeditionSpecFactory, Platform, PlatformType,
+    WebmateAPISession,
+} from "webmate-sdk-js";
+import {
+    MY_WEBMATE_APIKEY,
+    MY_WEBMATE_PROJECTID,
+    MY_WEBMATE_USER, WEBMATE_API_URL
+} from "./credentials";
+import * as Webmate from "webmate-sdk-js";
+import {mergeMap, tap} from "rxjs/operators";
+should();
+
+describe('URL Based Crossbrowser Test', function () {
+
+    let log = logger('tests:url-based-crossbrowser-test');
+
+    let webmateSession: WebmateAPISession;
+
+    // time out for test execution
+    this.timeout(600000);
+
+    before(function () {
+        // The user data can be configured in credentials.ts
+        webmateSession = Webmate.startSession(MY_WEBMATE_USER, MY_WEBMATE_APIKEY, WEBMATE_API_URL, MY_WEBMATE_PROJECTID);
+    });
+
+    it('should execute a crossbrowser test', async() => {
+        // Specify the reference browser
+        let referenceBrowser = new Browser(BrowserType.FIREFOX, "81", new Platform(PlatformType.WINDOWS, "10", "64"));
+
+        // Specify the browsers that should be compared to the reference browser
+        let crossBrowsers = [
+            new Browser(BrowserType.CHROME, "86", new Platform(PlatformType.WINDOWS, "10", "64")),
+            new Browser(BrowserType.INTERNET_EXPLORER, "11", new Platform(PlatformType.WINDOWS, "10", "64"))
+        ];
+
+        // TODO: do something with results
+
+        // Specify the urls under test
+        let urls = [
+            "http://www.examplepage.org/version/future",
+            "http://www.examplepage.org"
+        ];
+
+        let testExecutionSpecBuilder = ExpeditionComparisonCheckBuilder.builder(
+            "CrossBrowser Test via SDK",
+            ExpeditionSpecFactory.makeUrlListExpeditionSpec(urls, referenceBrowser),
+            [...(crossBrowsers.map(browser => ExpeditionSpecFactory.makeUrlListExpeditionSpec(urls, browser)))]
+        ).withTagName("SDK").withTagName("Release", "2020-11");
+
+        await webmateSession.testMgmt.startExecutionWithBuilder(testExecutionSpecBuilder).pipe(
+            mergeMap(testRun => {
+                return testRun.waitForCompletion();
+            }),
+            tap(info => {
+                log.info(`Finished waiting for TestRun: ${info.toString()}`);
+                log.info(`The result is available at: https://app.webmate.io/#/projects/${info.projectId}/testlab/testruns/${info.testRunId}`);
+            })
+        ).toPromise();
+    });
+
+});
